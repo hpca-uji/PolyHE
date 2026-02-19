6e8c6ab6864ad21ea556fde07c839bb0769b16f5
Miguel Ángel Prosper <mprosper@uji.es>
2026-02-19 16:03:32 +0100

    simplify tests

--- polyhe/test.py
+++ polyhe/test.py
@@ -12,8 +12,6 @@ __all__ = ()
 
 def main(config: Namespace) -> None:
     ctx = polyhe.new(config.backend)
-    rel_tol = 1e-8
-    abs_tol = 1e-8
 
     # Numbers
     p1 = 1
@@ -23,7 +21,7 @@ def main(config: Namespace) -> None:
     c3 = c1 * c2 + p1
     p3 = ctx.decrypt(c3)
     ref = 0
-    assert np.isclose(p3, ref, rel_tol, abs_tol).all(), f"numbers error; got {p3}, expect {ref}"
+    assert np.isclose(p3, ref).all(), f"numbers error; got {p3}, expect {ref}"
 
     # Lists
     p1 = [0.3, 0.4]
@@ -33,7 +31,7 @@ def main(config: Namespace) -> None:
     c3 = -c1 + c2
     p3 = ctx.decrypt(c3)
     ref = [0.1, -0.1]
-    assert np.isclose(p3, ref, rel_tol, abs_tol).all(), f"lists error; got {p3}, expect {ref}"
+    assert np.isclose(p3, ref).all(), f"lists error; got {p3}, expect {ref}"
 
     # NumPy
     p1 = np.full((2, 3, 4), 0.4, np.float64)
@@ -43,7 +41,7 @@ def main(config: Namespace) -> None:
     c3 = c1 - c2
     p3 = ctx.decrypt(c3)
     ref = np.full((2, 3, 4), 0.1, np.float64)
-    assert np.isclose(p3, ref, rel_tol, abs_tol).all(), f"numpy error; got {p3}, expect {ref}"
+    assert np.isclose(p3, ref).all(), f"numpy error; got {p3}, expect {ref}"
 
 
 if __name__ == "__main__":
